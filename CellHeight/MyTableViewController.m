6188cc7cdbd5141623bad61041f8aee832517d8c
Takafumi Tamura <taktamur@gmail.com>
2015-06-28 17:44:38 +0900

    after.

--- CellHeight/MyTableViewController.m
+++ CellHeight/MyTableViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "MyTableViewController.h"
-
+#import "MyTableViewCell.h"
 @interface MyTableViewController ()
 @property(nonatomic)NSMutableArray *array;
+@property(nonatomic)MyTableViewCell *dummyCell;
 @end
 
 @implementation MyTableViewController
@@ -22,13 +23,18 @@
         NSUInteger rand =  arc4random()%5;
         [self.array addObject:@(rand)];
     }
-    self.tableView.estimatedRowHeight = 100;
+    // estimatedRowHeightは使わない。
+//    self.tableView.estimatedRowHeight = 100;
 //    self.tableView.rowHeight = UITableViewAutomaticDimension;
 
     self.refreshControl = [UIRefreshControl new];
     [self.refreshControl addTarget:self
                             action:@selector(refreshControlStateChanged:)
                   forControlEvents:UIControlEventValueChanged];
+    
+    // セルの高さを計算する用の、表示されないダミーのセルを用意する。
+    self.dummyCell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
 }
 
 - (void)refreshControlStateChanged:(id)sender
@@ -58,18 +64,6 @@
 
     });
 }
--(NSString *)stringByLineNumber:(NSUInteger)number
-{
-    NSString *ret = @"";
-    for(NSUInteger i=1; i<=number; i++ ){
-        ret = [ret stringByAppendingString:[@(number) description]];
-        if( i!=number){
-            ret = [ret stringByAppendingString:@"\n"];
-        }
-    }
-    return ret;
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -87,21 +81,69 @@
     return self.array.count;
 }
 
+/**
+ Autolayoutを使ったUITableViewCellの高さ計算
+ */
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 高さ計算用のダミーのセルに、cellForRowAtIndexPathで行うのと
+    // 同じ設定をする。
+    [self setupCell:self.dummyCell atIndexPath:indexPath];
+
+    // 以下、サイズ計算用の処理です。
+    // ダミーセルそのものに操作しても、サイズ計算は行われないので、
+    // その内側にあるcontentViewに対して操作を行います。
+    // contentViewを再レイアウトする
+    [self.dummyCell.contentView setNeedsLayout];
+    [self.dummyCell.contentView layoutIfNeeded];
+    // contentViewの、Autolayoutに従った時のサイズを取得する
+    CGSize size = [self.dummyCell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+    // UITableViewの高さは、contentViewと「区切り線」を含んだ高さなので、
+    // 高さに区切り線分の高さを+1して返す
+    return size.height+1;
+}
+
+/** MyTableViewCellの設定処理 */
+// MyTableViewCellへの設定処理が重たい場合は、
+// 「高さ計算で必要な設定」と「高さ計算では不要な設定」に
+// 分けたほうが良いかもしれません。
+-(void)setupCell:(MyTableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
+{
+    NSUInteger num = (NSUInteger)[self.array[indexPath.row] integerValue];
+    cell.title.text = [self stringByLineNumber:num];
+    cell.backgroundColor = [self colorByNumber:num];
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     NSLog( @"cellForRowAtIndexPath: row=%@",@(indexPath.row));
+    MyTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"
+                                                            forIndexPath:indexPath];
+    [self setupCell:cell atIndexPath:indexPath];
+//    NSLog( @"cell=%@",cell);
+    return cell;
+}
+
+#pragma mark - ユーティリティ
+-(UIColor *)colorByNumber:(NSUInteger)number
+{
     NSArray *cellColors= @[ [UIColor whiteColor],
                             [UIColor redColor],
                             [UIColor blueColor],
                             [UIColor yellowColor],
                             [UIColor greenColor]];
-
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"
-                                                            forIndexPath:indexPath];
-    NSUInteger num = (NSUInteger)[self.array[indexPath.row] integerValue];
-    cell.textLabel.text = [self stringByLineNumber:num];
-    cell.backgroundColor = cellColors[num];
-    [cell layoutIfNeeded];
-    return cell;
+    
+    return cellColors[number%5];
+}
+-(NSString *)stringByLineNumber:(NSUInteger)number
+{
+    NSString *ret = @"";
+    for(NSUInteger i=1; i<=number; i++ ){
+        ret = [ret stringByAppendingString:[@(number) description]];
+        if( i!=number){
+            ret = [ret stringByAppendingString:@"\n"];
+        }
+    }
+    return ret;
 }
 
 
